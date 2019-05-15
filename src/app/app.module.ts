503160ecb25143907f2a76276902b34642470674
MaciejGolowkin <maciek.golowkin@gmail.com>
2019-05-15 15:37:37 +0200

    UploadedMap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
+import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
+import { MapComponent } from './Components/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: ''
+    })
   ],
   providers: [],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
