d88c483f2412d23d02ed4470f203a3a3be0467e5
Nadia <nadia.ruszkiewicz@gmail.com>
2019-05-15 18:58:47 +0200

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { AgmCoreModule } from '@agm/core';
 import { AppComponent } from './app.component';
 import { MapComponent } from './Components/map/map.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     CommonModule,
+    NgbModule,
     FormsModule,
     AgmCoreModule.forRoot({
       apiKey: ''
