06cfc78d8bdb0c746af0618bc8b83ce6f7324301
MaciejGolowkin <maciek.golowkin@gmail.com>
2019-05-15 17:24:13 +0200

    Basic operations on markers

--- src/app/Components/map/map.component.ts
+++ src/app/Components/map/map.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+
+
+
+  title = 'ProjektChmura';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  selectedMarker;
+
+  markers = [
+    // These are all just random coordinates from https://www.random.org/geographic-coordinates/
+    { lat: 22.33159, lng: 105.63233, alpha: 1 },
+    { lat: 7.92658, lng: -12.05228, alpha: 1 },
+    { lat: 48.75606, lng: -118.859, alpha: 1 },
+    { lat: 5.19334, lng: -67.03352, alpha: 1 },
+    { lat: 12.09407, lng: 26.31618, alpha: 1 },
+    { lat: 47.92393, lng: 78.58339, alpha: 1 }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  addMarker(lat: number, lng: number) {
+    this.markers.push({ lat, lng, alpha: 0.4 });
+  }
+
+  max(coordType: 'lat' | 'lng'): number {
+    return Math.max(...this.markers.map(marker => marker[coordType]));
+  }
+
+  min(coordType: 'lat' | 'lng'): number {
+    return Math.min(...this.markers.map(marker => marker[coordType]));
+  }
+
+  selectMarker(event) {
+    this.selectedMarker = {
+      lat: event.latitude,
+      lng: event.longitude
+    };
   }
 
 }
